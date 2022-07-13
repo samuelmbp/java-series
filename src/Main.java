3d9adfba998ef03f8cf9e72b3b6d0ae21bceab8c
Samuel Raducan <samuel.thembp@gmail.com>
2022-07-13 12:38:16 +0100

    Java: reference type vs primitive type

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.util.Date;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        byte age  = 30;
+        // PRIMITIVE TYPES
+        byte age = 30;
         long viewsCount = 3_123_456_789L;
         float price = 10.99F;
         char letter = 'a';
         boolean isEligible = true; // or false
+
+        // REFERENCE TYPES
+        Date now = new Date();
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2); // 2, 1
     }
 }
