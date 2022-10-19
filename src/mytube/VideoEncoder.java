61ec2461e7474bc80e073e0685cd0443396dc872
Samuel Raducan <samuel.thembp@gmail.com>
2022-10-19 13:06:06 +0100

    Add mytube project setup - needs refactoring

---
+++ src/mytube/VideoEncoder.java
@@ -0,0 +1,8 @@
+package mytube;
+
+public class VideoEncoder {
+    public void encode(Video video) {
+        System.out.println("Encoding video...");
+        System.out.println("Done!\n");
+    }
+}
