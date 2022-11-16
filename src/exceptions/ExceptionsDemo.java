09e4b4a2a8df5bb1b5ba1763b224e766885a23d7
Samuel Raducan <samuel.thembp@gmail.com>
2022-11-16 10:38:25 +0000

    Exceptions: try with resources statement

--- src/exceptions/ExceptionsDemo.java
+++ src/exceptions/ExceptionsDemo.java
@@ -2,6 +2,7 @@ package exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -50,21 +51,14 @@ public class ExceptionsDemo {
 //        }
 
         // Handle Multiple Exceptions
-        FileReader reader = null;
-        try {
-            reader = new FileReader("file.txt");
+        try (
+            FileReader reader = new FileReader("file.txt");
+            FileWriter writer = new FileWriter("...")
+            ) { //  <- try with resources statement
             int value = reader.read();
             new SimpleDateFormat().parse("");
         } catch (IOException | ParseException e) {
             System.out.println("Could not read data.");
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
     }
 
