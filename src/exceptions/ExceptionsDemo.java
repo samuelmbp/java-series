b2143cc47fef58f1b7f950e0b6d61d0dc5d5b021
Samuel Raducan <samuel.thembp@gmail.com>
2022-11-16 09:58:58 +0000

    Catching checked exceptions with try catch block

--- src/exceptions/ExceptionsDemo.java
+++ src/exceptions/ExceptionsDemo.java
@@ -1,16 +1,17 @@
 package exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 /**
  * TYPES OF EXCEPTIONS:
  *
- * 1. Checked
+ * 1. Checked (EXCEPTION)
  *  -> a developer should anticipate and handle properly
  *  -> get checked at the compiled time
  *
- * 2. Unchecked
- *  -> runtime exceptions -> occurs at runtime
+ * 2. Unchecked (RUNTIME EXCEPTIONS)
+ *  -> occurs at runtime
  *
  * 3. Error
  *  -> external to our application
@@ -18,17 +19,29 @@ import java.io.FileReader;
  * */
 
 public class ExceptionsDemo {
+//    public static void show() {
+//        // TODO: Checked Exceptions
+//        // File does not exist
+//        FileReader reader = new FileReader("file.txt");
+//
+//        // TODO: Unchecked Exceptions
+//        // Prevent by writing tests and handling them
+//        sayHello(null);
+//
+//        // TODO: Error Exceptions
+//        //
+//    }
+
+    // Handle Checked Exceptions
     public static void show() {
-        // TODO: Checked Exceptions
-        // File does not exist
-        FileReader reader = new FileReader("file.txt");
+        try {
+            FileReader reader = new FileReader("file.txt");
+            System.out.println("File opened.");
+        } catch (FileNotFoundException ex) {
+            System.out.println("File does not exist.");
+            System.out.println(ex.getMessage());
+        }
 
-        // TODO: Unchecked Exceptions
-        // Prevent by writing tests and handling them
-        sayHello(null);
-
-        // TODO: Error Exceptions
-        //
     }
 
     public static void sayHello(String name) {
