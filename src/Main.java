6f95dacb023540623144f6f30bab89ee045114ef
Samuel Raducan <samuel.thembp@gmail.com>
2022-11-23 18:26:29 +0000

    Java: Comparable Interface - sort data

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-import collections.ListDemo;
+import collections.Customer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ListDemo.show();
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("b"));
+        customers.add(new Customer("a"));
+        customers.add(new Customer("c"));
+        Collections.sort(customers);
+        System.out.println(customers);
     }
 }
