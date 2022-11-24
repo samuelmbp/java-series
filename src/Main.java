a6838578534d205d6cfea575637bda334f8274be
Samuel Raducan <samuel.thembp@gmail.com>
2022-11-24 17:29:45 +0000

    Java: Comparator interface

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import collections.Customer;
+import collections.EmailComparator;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,10 +8,11 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Customer> customers = new ArrayList<>();
-        customers.add(new Customer("b"));
-        customers.add(new Customer("a"));
-        customers.add(new Customer("c"));
-        Collections.sort(customers);
+        customers.add(new Customer("b", "e3"));
+        customers.add(new Customer("a", "e2"));
+        customers.add(new Customer("c", "e1"));
+//        Collections.sort(customers, new EmailComparator());
+        customers.sort(new EmailComparator()); // shorter syntax
         System.out.println(customers);
     }
 }
