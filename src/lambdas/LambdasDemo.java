75183ba8c7e93e3b35d6e4831bb81238d0db8ffd
Samuel Raducan <samuel.thembp@gmail.com>
2022-11-27 19:12:01 +0000

    Java: Lambda method references

--- src/lambdas/LambdasDemo.java
+++ src/lambdas/LambdasDemo.java
@@ -1,12 +1,27 @@
 package lambdas;
 
 public class LambdasDemo {
-    public static void show() {
-        // Lambda Expression
-        greet(message -> System.out.println(message));
+    public static String prefix = "-";
 
-        // Store lambda expression in a variable
-        Printer printer = message -> System.out.println(message);
+    public LambdasDemo(String message) {}
+
+    public static void print(String message) {
+    }
+
+    public static void show() {
+        // Lambda Expression -> Compact and easier to read expressions
+        greet(message -> System.out.println(prefix + message));
+
+        /* Method References -> shorter */
+//        LambdasDemo demo = new LambdasDemo();
+        greet(System.out::println);
+        greet(LambdasDemo::print); // static methods
+//        greet(this::print); // class instance method (no static)
+//        greet(demo::print); // class instance method (no static)
+
+        // Constructor reference
+        greet(message -> new LambdasDemo(message));
+        greet(LambdasDemo::new); // shorter
     }
 
     public static void greet(Printer printer) {
