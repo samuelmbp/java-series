7e113c3de1408dc409b8030a7727322e40eebe74
Samuel Raducan <samuel.thembp@gmail.com>
2022-11-29 17:55:00 +0000

    Java: creating a stream

--- src/streams/StreamsDemo.java
+++ src/streams/StreamsDemo.java
@@ -1,6 +1,9 @@
 package streams;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 /* Streams: Process a collection of data in a declarative/functional way */
 public class StreamsDemo {
@@ -22,5 +25,20 @@ public class StreamsDemo {
                 .stream()
                 .filter(movie -> movie.getLikes() > 10)
                 .count();
+
+        /* Creating a stream */
+
+        // Collection
+        ArrayList<String> list = new ArrayList<>();
+//        list.stream();
+
+        int[] numbers = { 1,2,3 };
+        Arrays.stream(numbers).forEach(number -> System.out.println(number));
+
+        // Generate unlimited numbers / or limit
+        Stream.generate(() -> Math.random()).limit(10).forEach(number -> System.out.println(number)); // One way
+
+        // Unary operator: takes a value and returns a new value
+        Stream.iterate(1, number -> number + 1).limit(10).forEach(number -> System.out.println(number)); // numbers 1...10
     }
 }
